0a5db42ed17307889e5fc02846f71f22b9cd609b
david <kaneshiuchiha@gmail.com>
2024-09-22 12:38:24 -0500

    second firebase commit

--- firebase.js
+++ firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import './envConfig.js'
 
 
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: "233057066502",
   appId: "1:233057066502:web:f0048bbd521e82cc89ed87",
   measurementId: "G-KQRZBTVRY1"
